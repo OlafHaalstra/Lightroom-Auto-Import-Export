94737ff35fb192da7837740365624ad444df6948
Chaochana Siparitat <chaochana@gmail.com>
2023-09-03 12:56:43 +0700

    update menu and delete unused file

--- autoimportexport.lrdevplugin/Info.lua
+++ autoimportexport.lrdevplugin/Info.lua
@@ -3,17 +3,14 @@ return {
 	LrSdkVersion = 3.0,
 	LrSdkMinimumVersion = 1.3, -- minimum SDK version required by this plug-in
 
-	LrToolkitIdentifier = 'org.365labs.app.pixid.autoimportexport',
+	LrToolkitIdentifier = 'org.365labs.app.pixid.autopresetsavetodisk',
 
-	LrPluginName = LOC "$$$/AutoImportExport/PluginName=Pixid Auto Import & Export ",
+	LrPluginName = LOC "$$$/AutoImportExport/PluginName=Pixid Save-to-Disk Plug-in",
 
 	LrExportMenuItems = {{
-		title = "Auto Import & Export resize 2000px",
+		title = "Save-to-disk Console ...",
 		file = "ExportMenuItem.lua",		
-	},{
-		title = "Auto Import & Export fullsize",
-		file = "ExportMenuItemFullsize.lua",		
-	},},
+	}},
 	VERSION = { major=1, minor=0, revision=0, build="20220724", },
 
 }
